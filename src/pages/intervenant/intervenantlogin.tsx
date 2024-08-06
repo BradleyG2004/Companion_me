f1d9ed628644c322cb1532f64cbc37c391516ee2
BradleyG2004 <129465283+BradleyG2004@users.noreply.github.com>
2024-08-06 16:48:04 +0200

    remove unused import in intervenant login

--- src/pages/intervenant/intervenantlogin.tsx
+++ src/pages/intervenant/intervenantlogin.tsx
@@ -1,6 +1,6 @@
 // src/pages/IntervenantLogIn.tsx
-import React, { useEffect, useState } from 'react';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+// import { Link, NavLink, useNavigate } from 'react-router-dom';
 import '../../global.css';
 
 const IntervenantLogIn: React.FC = () => {
